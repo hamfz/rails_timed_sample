014e9b400e9a9789100badacb9b82e828b8f34e6
ham <hfancherr@gmail.com>
2018-02-16 20:40:05 -0700

    OCD styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
